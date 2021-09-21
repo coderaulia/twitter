44d0ba5bb2d4cf4098f518980d21a3a5a3d75ee6
coderaulia <auliasw89@gmail.com>
2021-09-22 04:59:01 +0700

    setting up sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./index.css";
+import Sidebar from "./parts/Sidebar";
 
 function App() {
 	return (
@@ -7,6 +8,7 @@ function App() {
 			<h1>Twitter clone</h1>
 
 			{/* Sidebar */}
+			<Sidebar />
 			{/* Timeline */}
 			{/* Widgets */}
 		</div>

49bde465021fda53098712e2f34c3d867fbe236b
coderaulia <auliasw89@gmail.com>
2021-09-22 04:07:34 +0700

    init project

---
+++ src/parts/Sidebar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+	return (
+		<div className="sidebar">
+			{/* Twitter icon */}
+
+			{/* Options */}
+			{/* Options */}
+			{/* Options */}
+			{/* Options */}
+			{/* Options */}
+			{/* Options */}
+			{/* Tweet button */}
+		</div>
+	);
+}
+
+export default Sidebar;
